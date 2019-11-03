fec976f4c3cc5d9ee2314acefeac3f5145ce76be
Marina Moreira <38846018+MarinaFX@users.noreply.github.com>
2019-11-03 10:40:41 -0300

    INFERNO DE GIT

--- src/roteador/TabelaRoteamento.java
+++ src/roteador/TabelaRoteamento.java
@@ -5,95 +5,84 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TabelaRoteamento {
-    /*Implemente uma estrutura de dados para manter a tabela de roteamento. 
+    /*Implemente uma estrutura de dados para manter a tabela de roteamento.
      * A tabela deve possuir: IP Destino, Métrica e IP de Saída.
-    */
-
+     */
     private List<Endereco> tabelaRoteamento;
     private boolean modificada;
 
-    public TabelaRoteamento(){
+    public TabelaRoteamento() {
         tabelaRoteamento = new ArrayList<>();
         modificada = false;
     }
-    
-    
-    public void update_tabela(String tabela_s,  InetAddress IPAddress){
+
+
+    public void update_tabela(String tabela_s, InetAddress IPAddress) {
         /* Atualize a tabela de rotamento a partir da string recebida. */
+        String tabelaString;
+        tabelaString = tabela_s.trim();
 
-        if(tabela_s.equals("!")) {
-
-            Endereco end = new Endereco(IPAddress.toString(),"1");
+        if (tabelaString.equals("!")) {
+            Endereco end = new Endereco(IPAddress.getHostAddress(), "1");
             tabelaRoteamento.add(end);
             modificada = true;
-
-        } else {
-
-            List<Endereco> tabelaRecebida = getTabelaRecebida(tabela_s);
-            addNovasRotas(tabelaRecebida,IPAddress.toString());
-
+        }
+        else {
+            List<Endereco> tabelaRecebida = getTabelaRecebida(tabelaString);
+            addNovasRotas(tabelaRecebida, IPAddress);
         }
 
-        System.out.println( IPAddress.getHostAddress() + ": " + tabela_s);
+        System.out.println(IPAddress.getHostAddress() + ": " + tabela_s);
     }
 
-    public void addNovasRotas(List<Endereco> tabelaRecebida, String ipSaida) {
-
-        for(Endereco end : tabelaRecebida) {
-
-            if(!tabelaRoteamento.contains(end)) {
-                end.setIpSaida(ipSaida);
-                tabelaRoteamento.add(end);
+    public void addNovasRotas(List<Endereco> tabelaRecebida, InetAddress IPSaida) {
+        for (Endereco end : tabelaRecebida) {
+            if (!tabelaRoteamento.contains(end.toString())) {
+                end.setIpSaida(IPSaida.getHostAddress());
+                //tabelaRoteamento.add(end);
                 modificada = true;
             }
-
         }
-
     }
 
     public List<Endereco> getTabelaRecebida(String tabela_s) {
-
         List<Endereco> tabelaRecebida = new ArrayList<>();
 
         String[] end1 = tabela_s.split("\\*");
 
-        for(int i=0; i<end1.length; i++) {
-
-            String[] enderecosEmetricas = end1[i].split(";");
-
-            tabelaRecebida.add(new Endereco(enderecosEmetricas[i],enderecosEmetricas[i++]));
-
+        for (int i = 0; i < end1.length; i++) {
+            if (!end1[i].equals("")){
+                String[] enderecosEmetricas = end1[i].split(";");
+                tabelaRecebida.add(new Endereco(enderecosEmetricas[0], enderecosEmetricas[1]));
+            }
         }
 
         return tabelaRecebida;
     }
 
-    
-    public String get_tabela_string(){
 
+    public String get_tabela_string() {
+        modificada = false;
         String tabela_string = "";
 
-        if(tabelaRoteamento.size() == 0) {
-
+        if (tabelaRoteamento.size() == 0) {
             /* Tabela de roteamento vazia conforme especificado no protocolo */
             tabela_string = "!";
-
         } else {
-
             /* Converta a tabela de rotamento para string, conforme formato definido no protocolo . */
             StringBuilder sb = new StringBuilder();
 
             for (Endereco end : tabelaRoteamento) {
                 sb.append(end.toString());
-                sb.append("\n");
+                sb.append("\\n");
             }
-
             tabela_string = sb.toString();
         }
 
         return tabela_string;
     }
-    
 
-    
+    public boolean getEstado() {
+        return modificada;
+    }
 }
